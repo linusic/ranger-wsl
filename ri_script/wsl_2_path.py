689d9838618d5fcc22969eb79ba943a5bb800d89
lin <Cython_lin@cklin.top>
2022-05-29 13:42:02 +0800

    replace with rel path

--- ri_script/wsl_2_path.py
+++ ri_script/wsl_2_path.py
@@ -1,5 +1,7 @@
 import sys
+from pathlib import Path
 
 for file_path in sys.argv[1:]:
-    path = f'{file_path[5]}:/{file_path[7:]}'
-    print(f'"{path}"')
+#    path = f'{file_path[5]}:/{file_path[7:]}'
+#    print(f'"{path}"')
+    print(f'"{Path(file_path).name}"')
